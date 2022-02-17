04345fb9c2e8ceb87e47a1378467a331a718466c
anst9000 <coltlaboy@yahoo.se>
2022-02-17 16:38:25 +0100

    Create new project

---
+++ guava-example/src/main/java/com/coltla/examples/guava_example/App.java
@@ -0,0 +1,23 @@
+package com.coltla.examples.guava_example;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Multiset<String> animals = HashMultiset.create();
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("cat");
+        
+        animals.forEach(System.out::println);
+    }
+}
